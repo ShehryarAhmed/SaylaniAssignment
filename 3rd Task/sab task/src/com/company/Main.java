90f64d7f50ee9bd73693ffd03d44826d9ba7b084
shehryarahmed <shehreyarahmed5@gmail.com>
2016-05-30 23:58:48 +0500

    Third sab Task Complete..

--- 3rd Task/sab task/src/com/company/Main.java
+++ 3rd Task/sab task/src/com/company/Main.java
@@ -4,12 +4,21 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-    char getnum;
-    int firstval,second ;
+        char getnum;
+        int firstval,second ;
+        String Ones[] = {"Zero","one","two","three","four","five","six","seven","eight","nine"};
+        String ten[] = {"one","ten","twenty","thirty","fourty","fivety","sixty","seventy","eighty","ninety","hundred"};
+
         Scanner getval = new Scanner(System.in);
         System.out.println("Enter Number Like 25 ,15...");
-        getnum = getval.next().charAt(0);
-        firstval = getnum;
-        System.out.println(firstval);
+        int get = getval.nextInt();
+        char  cast[] = String.valueOf(get).toCharArray();
+        firstval = Integer.valueOf(cast[0]);
+        second = Integer.valueOf(cast[1]);
+        firstval -= 48;
+        second -= 48;
+
+        System.out.println(get + " : " + ten[firstval] + Ones[second]);
+        System.out.println(firstval + " "+second);
     }
 }
